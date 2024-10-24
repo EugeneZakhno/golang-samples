7198fcaae20790af3eac7d11484e12decb74a233
Eugene <30446748+EugeneZakhno@users.noreply.github.com>
2024-10-24 16:52:07 +0300

    init

---
+++ GoshaDudar.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello World")
+	variables()
+}
+
+// Variables
+func variables() {
+	//Variables_1
+	var age = 38
+	var name = "Zhenia"
+	var num = 0.47
+	var zero float32 = 45.55648484
+	var float float64 = 5444.55151
+	fmt.Println(age, name, num, zero, float)
+}
